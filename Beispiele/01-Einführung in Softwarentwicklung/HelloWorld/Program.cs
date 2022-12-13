3817250e72ef7b21387caedce8e681478e1888ba
Marcel Peduzzi <marcel.peduzzi@egeli-informatik.ch>
2022-12-13 18:59:03 +0100

    Hello World Projekt hinzugefügt

---
+++ Beispiele/01-Einführung in Softwarentwicklung/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Dein Name: ");
+        var name = Console.ReadLine();
+        Console.WriteLine("Hi " + name + ", willkommen in der Welt des Programmierens.");
+        Console.WriteLine("");
+        Console.WriteLine("Zum beenden Taste drücken");
+        Console.ReadKey();
+    }
+}
+
