652ca1bdef71f84d41551fac46b258f60945d822
Marcel Peduzzi <marcel.peduzzi@egeli-informatik.ch>
2022-12-20 17:53:38 +0100

    Veriablen und Datentypen

---
+++ Uebungen/02-Variablen und Datentypen/Datentypen/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UDatentypen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdAnzeigen_Click(object sender, EventArgs e)
+        {
+            // Dein Code ...
+
+            //    LblAnzeige.Text = "Adresse: " + "\n" + vorname + " " + nachname +
+            //        "\n" + strasse + "\n" + plz + " " + ort + "\n" + "\n" +
+            //        "Alter: " + alter + "\n" + "Gehalt: " + gehalt;
+        }
+    }
+}
