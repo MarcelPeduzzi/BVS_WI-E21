460c1c9e1c373ef1dcbad102df3c4e318a828e9a
Marcel Peduzzi <marcel.peduzzi@egeli-informatik.ch>
2022-12-21 20:45:38 +0100

    Loesung Uebung 3

---
+++ Loesungen/03-Operatoren/LogischeOperatoren/net Framework/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ULogischeOperatoren
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdAnzeigen1_Click(object sender, EventArgs e)
+        {
+            var p = 4 > 3 && -4 > -3;
+
+            LblAnzeige.Text = "Ergebnis: " + p;
+        }
+
+        private void CmdAnzeigen2_Click(object sender, EventArgs e)
+        {
+            bool p;
+            p = 4 > 3 || -4 > -3;
+            bool c;
+            c = true || false;
+            LblAnzeige.Text = "Ergebnis: " + p;
+        }
+    }
+}
