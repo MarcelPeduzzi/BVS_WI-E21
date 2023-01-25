08e7f8f51ddd465634ab79f964863cdf7e45e038
Marcel Peduzzi <marcel.peduzzi@egeli-informatik.ch>
2023-01-25 20:08:37 +0100

    Beispiele 08-Vererbung

---
+++ Beispiele/08-Vererbung/MethodenOverriding/KundenSample/Kunde.cs
@@ -0,0 +1,29 @@
+﻿namespace KundenSample
+{
+    public class Kunde
+    {
+        public string Anrede { get; set; }
+        public string Nachname { get; set; }
+        public bool StammKunde { get; set; }
+        public decimal Guthaben { get; protected set; }
+
+        public Kunde(string anrede, string nachname)
+        {
+            Anrede = anrede;
+            Nachname = nachname;
+        }
+
+        public virtual string GetAdresse()
+        {
+            return $"{Anrede} {Nachname}";
+        }
+
+        public virtual void AddGuthaben(decimal betrag)
+        {
+            if (StammKunde)
+            {
+                Guthaben += betrag;
+            }
+        }
+    }
+}
---
+++ Beispiele/08-Vererbung/MethodenOverriding/KundenSample/PrivateKunde.cs
@@ -0,0 +1,26 @@
+﻿namespace KundenSample
+{
+    public class PrivateKunde : Kunde // erbt von der Basisklasse
+    {
+        public string Wohnort { get; set; }
+
+        public PrivateKunde(string anrede, string name, string wohnort) : base(anrede, name)
+        {
+            Wohnort = wohnort; // Klassenspezifische Ergänzung
+        }
+
+        public override string GetAdresse()
+        {
+            const char LF = (char)10; // Zeilenvorschung
+            return (base.GetAdresse() + LF + Wohnort);
+        }
+
+        public override void AddGuthaben(decimal betrag)
+        {
+            const decimal bonus = (decimal)0.05;
+
+            // Zugriff auf proteced-Property in Basisklasse:
+            base.Guthaben += bonus * betrag;
+        }
+    }
+}
